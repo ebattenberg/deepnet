39fba90e50e30f693baf41252192d0a1dfb99642
ebattenberg <ebattenberg@gmail.com>
2013-07-05 20:54:57 -0700

    update gitignore and proto configs

--- cudamat_conv/build.sh
+++ cudamat_conv/build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-export CUDA_SDK_PATH=$HOME/NVIDIA_GPU_Computing_SDK
-export CUDA_INSTALL_PATH=/pkgs_local/cuda-4.2/
+#export CUDA_SDK_PATH=/usr/local/cuda/
+export CUDA_SDK_PATH=/home/ericb/NVIDIA_CUDA-5.0_Samples/common/inc
+export CUDA_INSTALL_PATH=/usr/local/cuda/
 export PYTHON_INCLUDE_PATH=/usr/include/python2.7/
 export NUMPY_INCLUDE_PATH=/usr/include/python2.7/numpy/
 #export ATLAS_LIB_PATH=/usr/lib/atlas-base/atlas
